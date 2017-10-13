eee6ab1e54b19b00b9dbd4830f7fcb5aa0a530c3
androvolk <noname@nowhere.net>
2017-10-13 16:21:37 +0300

    Feed utility class added.

---
+++ Utils/src/test/lambda/utils/Feed.java
@@ -0,0 +1,66 @@
+package test.lambda.utils;
+
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public final class Feed
+{
+  private static final String STATUS_OK = "{\n\t\"status\": \"success\"\n\t\"success\": \"true\"\n}";
+  private static String xmlProvHost = "127.0.0.1";
+  private static String xmlProvPort = "8080";
+//  private static String dbHost = "127.0.0.1";
+//  private static int dbPort = 5984;
+//  private static String dbName = "feed_files";
+//  private static String dbLogin = "lambda_demo";
+//  private static String dbPassword = "123456";
+
+  
+   
+  public static String create ( final JsonObject args ) throws IOException
+  {
+    Gson gson = null;
+    String response = null;
+    String jsonDocument = null;
+ 
+    System.out.print ( "Creating feed..." );
+
+    if ( args.has ( "xmlProvHost" ) ) xmlProvHost = args. get ( "xmlProvHost" ) .getAsString ();
+    if ( args.has ( "xmlProvPort" ) ) xmlProvPort = args. get ( "xmlProvPort" ) .getAsString ();
+    
+    gson = new Gson ();
+    jsonDocument = gson.toJson ( args );
+    
+System.out.println ( "JSON -> " + jsonDocument );//!!!
+
+    response = CallRestService.put ( "http://" + xmlProvHost + ":" +  xmlProvPort + "/config",  jsonDocument  );
+
+    if ( response == null )
+    {
+      System.err.println ( "Feed::create failed to configure new feed!" );
+      return null;
+    }
+
+System.out.println ( "response -> " + response );//!!!
+    
+    response = CallRestService.put ( "http://" + xmlProvHost + ":" +  xmlProvPort + "/register_trigger",  jsonDocument  );
+    
+    if ( response == null )
+    {
+      System.err.println ( "Feed::create failed to register trigger!" );
+      return null;
+    }
+
+System.out.println ( "response -> " + response );//!!!
+
+    return STATUS_OK;
+  }
+
+  public static String delete ( JsonObject args )
+  {
+    // TODO Implement feed deleting logic
+    return STATUS_OK;
+  }
+
+}
