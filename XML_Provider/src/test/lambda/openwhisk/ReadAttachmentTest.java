f1ad7a861834cc63af649910b07ec5b1a1ec4599
androvolk <noname@nowhere.net>
2017-10-26 17:04:28 +0300

    Attachment reading test.

---
+++ XML_Provider/src/test/lambda/openwhisk/ReadAttachmentTest.java
@@ -0,0 +1,33 @@
+package test.lambda.openwhisk;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Credentials;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public final class ReadAttachmentTest
+{
+
+  public static void main ( String [] args ) throws IOException
+  {
+    String postBody = "{}";
+    
+    Request request = new Request.Builder ()
+      .url ( "https://192.168.33.13:5984/feed_files/" + args [ 0 ] + "/xml?rev=" + args [ 1 ] )
+      .get ()
+      .build ();
+ 
+    Call call = new OkHttpClient () .newCall ( request );
+    Response response = call.execute ();
+    System.out.println ( "Headers -> " + response.headers () .toString () );
+    System.out.println ( "Code -> " + response.code () );
+    System.out.println ( "Successful -> " + response.isSuccessful () );
+    System.out.println ( response .body () .string () );
+  }
+
+}
