ab23266c3f718ba3d0cc0c8bacf92d62f73fd8ab
androvolk <noname@nowhere.net>
2017-11-03 22:45:10 +0300

    Testing schema in the jar.

--- Utils/src/test/lambda/utils/XmlValidator.java
+++ Utils/src/test/lambda/utils/XmlValidator.java
@@ -13,11 +13,15 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
 
 
 public final class XmlValidator
 {
+  private static final Logger log = LoggerFactory.getLogger ( XmlValidator.class );
+
   private static SchemaFactory factory = null;
   private static XMLInputFactory xmlFactory = null;
   
@@ -29,20 +33,30 @@ public final class XmlValidator
    */
   public static boolean isXmlValid ( final String xml, final String schemaFileName )
   {
+    log.debug ( String.format ( 
+          "Entering XmlValidator::isXmlValid()  Params: { xml = %s, schemaFileName = %s", xml, schemaFileName ) );
     try
     {
       if ( factory == null ) factory = SchemaFactory.newInstance ( XMLConstants.W3C_XML_SCHEMA_NS_URI );
+      log.debug ( String.format ( "factory = %s", factory ) );
       Schema schema = factory.newSchema ( new File ( schemaFileName ) );
+      log.debug ( String.format ( "schema = %s", schema ) );
       Validator validator = schema.newValidator ();
+      log.debug ( String.format ( "validator = %s", validator ) );
       if ( xmlFactory == null ) xmlFactory = XMLInputFactory.newFactory ();
+      log.debug ( String.format ( "xmlFactory = %s", xmlFactory ) );
       XMLEventReader reader = xmlFactory.createXMLEventReader ( new StringReader ( xml ) );
+      log.debug ( String.format ( "reader = %s", reader ) );
       validator.validate ( new StAXSource ( reader ) );
     }
     catch ( SAXException | IOException | XMLStreamException e )
     {
-      System.err.println ( "XmlValidator::isXmlValid found the problem in XML file: " + e.getMessage () );
+      log.error ( "XmlValidator::isXmlValid found the problem in XML file: " + e.getMessage () );
+      e.printStackTrace ();
       return false;
     }
+
+    log.debug ( "Successfully leaving Entering XmlValidator::isXmlValid()" );
     
     return true;
   }
